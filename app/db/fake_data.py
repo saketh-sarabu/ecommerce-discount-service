010403b5b74111c537177a1cd27896f8760b589f
Saketh Sarabu <107855794+saketh-sarabu@users.noreply.github.com>
2026-03-19 15:17:38 +0530

    Added new Discount rules to db in fake_data.py

--- app/db/fake_data.py
+++ app/db/fake_data.py
@@ -40,21 +40,30 @@ ICICI_PAYMENT = PaymentInfo(method="CARD", bank_name="ICICI", card_type=CardType
 
 BRAND_DISCOUNTS: list[BrandDiscount] = [
     BrandDiscount(name="PUMA 40% off", brand="PUMA", percent=Decimal("40")),
+    BrandDiscount(name="NIKE 30% off", brand="NIKE", percent=Decimal("30")),
+    BrandDiscount(name="ADIDAS 25% off", brand="ADIDAS", percent=Decimal("25")),
+    BrandDiscount(name="ZARA 20% off", brand="ZARA", percent=Decimal("20")),
 ]
 
 CATEGORY_DISCOUNTS: list[CategoryDiscount] = [
     CategoryDiscount(name="T-shirt extra 10%", category="t-shirts", percent=Decimal("10")),
+    CategoryDiscount(name="Footwear 15% off", category="footwear", percent=Decimal("15")),
+    CategoryDiscount(name="Jeans 20% off", category="jeans", percent=Decimal("20")),
+    CategoryDiscount(name="Accessories 5% off", category="accessories", percent=Decimal("5")),
 ]
 
 VOUCHERS: list[VoucherDiscount] = [
-    VoucherDiscount(
-        name="SUPER69",
-        code="SUPER69",
-        percent=Decimal("69"),
-        min_customer_tier=CustomerTier.REGULAR,
-    ),
+    VoucherDiscount(name="SUPER69", code="SUPER69", percent=Decimal("69"), min_customer_tier=CustomerTier.REGULAR),
+    VoucherDiscount(name="LOCALPROMO50", code="LOCALPROMO50", percent=Decimal("50"), min_customer_tier=CustomerTier.REGULAR, excluded_brands=["PUMA", "Nike"]),
+    VoucherDiscount(name="NOACCESSORIES60", code="NOACCESSORIES60", percent=Decimal("60"), min_customer_tier=CustomerTier.REGULAR, excluded_categories=["accessories"]),
+    VoucherDiscount(name="SILVER20", code="SILVER20", percent=Decimal("20"), min_customer_tier=CustomerTier.SILVER),
+    VoucherDiscount(name="GOLD50", code="GOLD50", percent=Decimal("50"), min_customer_tier=CustomerTier.GOLD, excluded_brands=["ZARA"], excluded_categories=["accessories"]),
+    VoucherDiscount(name="WELCOME10", code="WELCOME10", percent=Decimal("10"), min_customer_tier=CustomerTier.REGULAR)
 ]
 
 BANK_OFFERS: list[BankOffer] = [
     BankOffer(name="ICICI 10% instant", bank_name="ICICI", percent=Decimal("10")),
+    BankOffer(name="HDFC Credit 15%", bank_name="HDFC", percent=Decimal("15"), card_type=CardType.CREDIT),
+    BankOffer(name="SBI Debit 5%", bank_name="SBI", percent=Decimal("5"), card_type=CardType.DEBIT),
+    BankOffer(name="AXIS 12% instant", bank_name="AXIS", percent=Decimal("12")),
 ]
